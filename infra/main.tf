e3a0fff60893caab75f4e0908fd2eac40d39ad08
Andrei Enache <online@autgraph.com>
2026-03-22 11:37:09 +0100

    [INFRA] Terraform backend configured in terraform l

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,10 @@
 terraform {
-    
     required_providers {
         aws = {
             source  = "hashicorp/aws"
             version = "~> 4.0"
         }
     }
-
     
     backend "s3" {
         bucket = "inboxflow-terraform-state"
